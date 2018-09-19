d8c6947a09fb1a8111267cc5285987da1d8a0663
BrainWosher <svprokopenko92@gmail.com>
2018-09-19 20:50:29 +0700

    Revert "создание контейнера с помощью connect"
    This reverts commit 88eb7cb29bcb8510b8ad0531b6db8aba6440397b.

--- src/containers/ListContainer.jsx
+++ src/containers/ListContainer.jsx
@@ -1,29 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-
 import List from '../components/List';
+
 import { deleteTodo, toggleTodo, editTodo } from '../actions';
 
-// todos={this.context.store.getState()}
-// onDelete={}
-// onToggle={}
-// onEdit={}
+class ListContainer extends React.Component {
+    constructor(props, context) {
+        super(props, context);
 
-function mapStateToProps(state) {
-    return {
-        todos: state
-    };
+        this.store = this.context.store;
+
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleToggle = this.handleToggle.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+    }
+    componentDidMount() {
+        this.unsubscribe = this.store.subscribe(() => this.forceUpdate());
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+    
+    handleDelete(id) {
+        this.store.dispatch(deleteTodo(id));
+    }
+
+    handleToggle(id) {
+        this.store.dispatch(toggleTodo(id));
+    }
+
+    handleEdit(id, title) {
+        this.store.dispatch(editTodo(id, title));
+    }
+
+    render() {
+        const todos = this.store.getState();
+
+        return (
+            <List 
+                todos={todos}
+                onDelete={this.handleDelete}
+                onToggle={this.handleToggle}
+                onEdit={this.handleEdit}
+            />
+        );
+    }
 }
 
-function mapDispatchToProps(dispatch) {
-    return{
-        onDelete: id => dispatch(deleteTodo(id)),
-        onToggle: id => dispatch(toggleTodo(id)),
-        onEdit: (id, title) => dispatch(editTodo(id, title))
-    };
-}
-
-const ListContainer  = connect(mapStateToProps, mapDispatchToProps)(List);
+ListContainer.contextTypes = {
+    store: PropTypes.object
+};
 
 export default ListContainer;
