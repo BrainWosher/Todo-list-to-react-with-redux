034dc67a633659d42c9dd1c2c44134f68e187a12
Sergey <svprokopenko92@gmail.com>
2018-09-19 13:41:55 +0700

    выделил презинтационные компоненты и создал контейнеры

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-
 import reducer from './reducers';
 import todos from './todos';
 import App from './App';
-import { addTodo, deleteTodo, toggleTodo, editTodo } from './actions';
 
 const store = createStore(reducer, todos);
 
